33027cd1cee9bc8745401bf6b8404489b175512f
rodrgz456 <robertoandre0307@gamil.com>
2025-10-29 12:37:52 -0600

    feat: Se agrego la carpeta al repositorio

---
+++ JavaScript/Basic/01-variables.js
@@ -0,0 +1,11 @@
+// var
+var helloWorld = "Hola Mundo!"
+console.log(helloWorld)
+
+// let
+let helloWorld2 = "Hola JavaScript"
+console.log(helloWorld2)
+
+// const
+const constante = "Hola"
+console.log(constante)
