5c0f3e28ec140611fc20e266b290c6ecb0691fbc
rodrgz456 <robertoandre0307@gamil.com>
2025-10-29 16:50:02 -0600

    feat: Se agrego el archivo tiposDeDatos.js

---
+++ JavaScript/Basic/02-tiposDeDatos.js
@@ -0,0 +1,32 @@
+/* Tipos de datos Primitivos */
+
+// String (Cadena de texto)
+let String = "Esto es una cadena de texto"
+
+// Numeros
+let edad = 18 //Enteros
+let altura =  1.75 //Decimal
+
+// Booleanos
+let esHombre = true
+let esMujer = false
+
+// Undefined
+let undefinedValue //Aun no esta definida o no tiene un valor definido
+console.log(undefinedValue)
+
+// Null
+let nullValue = null
+
+// Symbol
+let mySymbol = Symbol("mysymbol") 
+
+// BigInt
+let myBigInt =  BigInt(123456897987654321132546) //Forma 1
+let myBigInt2 = 123456897987654321132546n //Forma 2
+
+
+//Mostrar los tipos de datos
+//typeof sirve para saber el tipo de datos de nuestra variable
+console.log(typeof edad)
+console.log(typeof nullValue)
